e9c9fb56661d1d5884f5f6fc5863404cbaf7ebfd
Muhammad Raihan Alifiandra <97264840+Re-kun@users.noreply.github.com>
2022-07-13 13:06:15 +0800

    Update header.php

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -8,7 +8,7 @@
         <title>IngfoBase | <?= $data['title'] ?></title>
 
         <link href="<?= BASEURL ?>/css/styles.css" rel="stylesheet" />
-        <link rel="stylesheet" type="text/css" href="<?= BASEURL ?>/css/card.css">        
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">      
     </head>
     <body>
         <!-- Responsive navbar-->
