bce7255731fac7740008791364e561378e84142f
Muhammad Raihan Alifiandra <97264840+Re-kun@users.noreply.github.com>
2022-07-13 13:07:33 +0800

    Update footer.php

--- app/views/layouts/footer.php
+++ app/views/layouts/footer.php
@@ -1,6 +1,6 @@
    <footer class="py-5 bg-dark">
         <div class="container px-5"><p class="m-0 text-center text-white">Copyright &copy; IngfoBase 2022</p></div>
-        </footer>
-        <script src="<?= BASEURL ?>/js/bootstrap.bundle.min.js"></script>
+   </footer>
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>      
     </body>
 </html>
